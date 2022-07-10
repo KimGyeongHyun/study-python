cc1bf08e98e7feb363dd823185417a1a1c034bca
KimGyeongHyun <105264179+KimGyeongHyun@users.noreply.github.com>
2022-07-10 17:56:41 +0900

    Add some information texts, 2-1.py
    2-1.py is not done yet

---
+++ final/2-1.py
@@ -0,0 +1,40 @@
+# Divide and Conquer 로 list 안 붙어있는 숫자 합 제일 큰 것 리턴
+
+def sublist_max_user(profits, start, end):
+
+    biggest_sum = profits[0]
+
+    if len(profits) == 1:
+        return biggest_sum
+    i, j = 0, 0
+
+    if len(profits) == 2:
+        mid = len(profits)//2
+        while i < len()
+
+        max(sublist_max_user(profits, start, mid),
+            sublist_max_user(profits, mid+1, end),
+            )
+
+
+
+def sublist_max(profits, start, end):
+
+
+
+
+
+
+
+# 테스트
+list1 = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(sublist_max(list1, 0, len(list1) - 1))
+
+list2 = [4, 7, -6, 9, 2, 6, -5, 7, 3, 1, -1, -7, 2]
+print(sublist_max(list2, 0, len(list2) - 1))
+
+list3 = [9, -8, 0, -7, 8, -6, -3, -8, 9, 2, 8, 3, -5, 1, -7, -1, 10, -1, -9, -5]
+print(sublist_max(list3, 0, len(list3) - 1))
+
+list4 = [-9, -8, -8, 6, -4, 6, -2, -3, -10, -8, -9, -9, 6, 2, 8, -1, -1]
+print(sublist_max(list4, 0, len(list4) - 1))
